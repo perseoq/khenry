a9354bafcd6338ff34f04849c6364de8cfcb4334
Rodrigo <rodrigoqyt@gmail.com>
2021-09-29 21:57:34 -0500

    Add files via upload

--- khenry.py
+++ khenry.py
@@ -6,8 +6,15 @@ from tkhtmlview import *
 
 
 def ventanaPrincipal():
+
+    def agregar():
+        print(variable.get())
+    
+    def borrar():
+        pass
+
     v = Tk()
-    v.title("Khenry - Base de Conocimientos")
+    v.title("Khenry - Base de Conocimientos v1.01")
     v.geometry('480x540')
     v.resizable(0,0)
     n = Notebook(v)
@@ -18,17 +25,20 @@ def ventanaPrincipal():
     tab1 = Frame(n)
     tab1.grid(row=0, column=0)
     tab2 = Frame(n)
-    tab2.grid(row=0, column=0)
+    tab2.grid(row=0, column=0, sticky='ne')
     tab3 = Frame(n)
     tab3.grid(row=0, column=0)
 
     tab4 = Frame(n)
     tab4.grid(row=0, column=0, sticky='ne')
+    tab5 = Frame(n)
+    tab5.grid(row=0, column=0, sticky='ne')
 
-    n.add(tab1, text="Portada ")
+    n.add(tab1, text="Portada")
     n.add(tab2, text="Útlimos Agregados")
     n.add(tab3, text="Buscar Documentos")
     n.add(tab4, text="Agregar Datos")
+    n.add(tab5, text=" + ")
 
     # Portada
 
@@ -44,8 +54,40 @@ def ventanaPrincipal():
     link_portada = HTMLLabel(tab1, html="<center><a href='http://helloperseo.party' style='font-size: 10px;'>helloperseo</a></center>")
     link_portada.grid(row=6, column=0)
     link_portada.config(height=2, width=9)
-    Label(tab1, text="El usuario es responsable por uso que le da a esta herramienta", font=("Georgia", 10)).grid(row=7, column=0)
+    Label(tab1, text="El usuario es responsable por uso que le da a esta herramienta", font=("Georgia", 10), foreground="#900C3F").grid(row=7, column=0)
     
+    # LISTAR ÚLTIMOS
+
+    Label(tab2, text=" ", width=20).grid(row=0, column=0, sticky='w', padx=10)
+    Label(tab2, text="Selecciona una categoría: ", width=31).grid(row=1, column=0, sticky='w', padx=10)
+    OptionList1 = ['Selecciona Categoría','Filosofía', 'Derecho', 'Matemáticas', 'Sociales', 'Física', 'Ciencia', 'Computación', 'Inteligencia Artificial', 'Ingeniería', 'Biología', 'Química', 'Lengua', 'Linguistica', 'Literatura']
+    var1 = StringVar(tab2)
+    var1.set(OptionList1[0])
+    CATEGORIA_VIEW = OptionMenu(tab2, var1, *OptionList1)
+    CATEGORIA_VIEW.grid(row=1, column=1, sticky='w', padx=10)
+    CATEGORIA_VIEW.config(width=18)
+    Label(tab2, text=" .", width=20).grid(row=2, column=0, sticky='w', padx=10)
+
+    # BUSCAR ELEMENTOS
+
+    Label(tab3, text="Buscar por título").grid(row=0, column=0, sticky="w", padx=10, pady=10)
+    titulo = Entry(tab3)
+    titulo.grid(row=0, column=1)
+    titulo.config(width=29)
+    Button(tab3, text="Buscar").grid(row=0, column=2)
+    Label(tab3, text="Buscar por etiqueta").grid(row=1, column=0, sticky="w", padx=10, pady=10)
+    etiqueta = Entry(tab3)
+    etiqueta.grid(row=1, column=1)
+    etiqueta.config(width=29)
+    Button(tab3, text="Buscar").grid(row=1, column=2)
+    Label(tab3, text="Buscar por autor").grid(row=2, column=0, sticky="w", padx=10, pady=10)
+    autor  = Entry(tab3)
+    autor.grid(row=2, column=1)
+    autor.config(width=29)
+    Button(tab3, text="Buscar").grid(row=2, column=2)
+
+
+
     # FORMULARIO DE AGREGAR DATOS TAB4
 
     Label(tab4, text="AGREGA NUEVO DOCUMENTO").grid(row=0, column=0, pady=10, columnspan=2)
@@ -88,7 +130,7 @@ def ventanaPrincipal():
     NOTAS = Entry(tab4)
     NOTAS.grid(row=21, column=0, columnspan=3, sticky='we', padx=10)
     Label(tab4, text="  ").grid(row=22, column=0, sticky='w', padx=10)
-    Button(tab4, text="Agregar").grid(row=23, column=0, columnspan=2, sticky='e', padx=10)
+    Button(tab4, text="Agregar", command=agregar).grid(row=23, column=0, columnspan=2, sticky='e', padx=10)
    
 
 
